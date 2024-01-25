3cc990258d7b494597a5aeac9b1d9dadc999f342
okiashi <110482361+okiashi@users.noreply.github.com>
2024-01-25 10:28:21 +0900

    Update OKAS_SaveChangeTint.js
    2024/01/25 Ver.1.1.0  色調の復元・予約実行時にウェイトを指定可能に。文言等の修正。利用規約の微改定。

--- OKAS_SaveChangeTint.js
+++ OKAS_SaveChangeTint.js
@@ -1,20 +1,22 @@
-//=================================================================================
-// OKAS_SaveChangeTint.js
-// 2022/08/13 Ver.1.0.1
-// twitter : https://twitter.com/misfit_okiashi
-//=================================================================================
+/* ==============================================================================
+OKAS_SaveChangeTint.js
+SNS： https://tm-misfit.hateblo.jp
+=================================================================================
+更新履歴：
+2024/01/25 Ver.1.1.0  色調の復元・予約実行時にウェイトを指定可能に。文言等の修正。利用規約の微改定。
+2022/08/08 Ver.1.0.0  初版
+*/
 /*:
  * @plugindesc 色調の保存、復元、予約
- * @target MZ
- * @url https://github.com/okiashi/RPGMakerMZ/blob/main/OKAS_SaveChangeTint.js
  * @author Okiashi
- * @help OKAS_SaveChangeTint.js
+ * @target MZ
+ * @url https://raw.githubusercontent.com/okiashi/RPGMakerMZ/main/OKAS_SaveChangeTint.js
+ * @help OKAS_SaveChangeTint.js (2024/01/25 Ver.1.1.0)
  *
  * 概要：
  * プラグインコマンドで色調の保存、復元、予約が行えます。
  * 「一時的に色調を変更し、ワンコマンドで元に戻したい」
  * 「色調のデフォルト設定のようなものを設けたい」 etc...
- * ちなみに戦闘中もプラグインコマンドが作動します。
  *
  * プラグインコマンド：
  * 　[パラメータ変数1]
@@ -34,13 +36,10 @@
  *
  * ----------------------------------------------------------------------------
  * 利用規約：
- * OK - 改変、再配布(無料)
- *      クレジット不要です。サポート対応できないため、ご自由に改変下さい。
- * NG - プラグイン単体の有料販売
- * ----------------------------------------------------------------------------
- * 更新履歴：
- * 2022/08/13 Ver.1.0.1　戦闘シーンでの動作をテストし、ヘルプ追記。
- * 2022/08/08 Ver.1.0.0　初版
+ * OK - 改変、プラグイン単体の再配布(無料)
+ *      サポート対応できないため、ご自由に改変下さい。クレジット不要です。
+ *      ご利用は自己責任でお願いいたします。
+ * NG - プラグイン単体の有料販売(丸ごと転載し販売などトラブルを招く行為)
  * ----------------------------------------------------------------------------
  *
  @ -------------------------- パラメータ
@@ -60,7 +59,7 @@
  *
  @ -------------------------- プラグインコマンド
  * @command SaveChangeTint
- * @text 現在の色調を変更&保存
+ * @text 現在の色調を保存し、変更
  * @desc 現在の色調を保存してから変更します。
  *
  * @arg Red
@@ -93,17 +92,22 @@
  * @desc ウェイト時間(フレーム：1/60秒)
  * @default 60
  *
- @ -------------------------- プラグインコマンド
+ @ --------------------------
  * @command SaveTint
  * @text 現在の色調を保存(保存のみ)
  * @desc 現在の色調を保存します。
  *
- @ -------------------------- プラグインコマンド
+ @ --------------------------
  * @command RestorationTint
  * @text 色調を復元
- * @desc 色調を変更前に戻します。
+ * @desc 色調を保存していた内容に戻します。
  *
- @ -------------------------- プラグインコマンド
+ * @arg Wait
+ * @text ウェイト時間(フレーム)
+ * @type string
+ * @desc ウェイト時間(フレーム:1/60秒)
+ * @default 60
+ @ --------------------------
  * @command SetTint
  * @text 色調を予約
  * @desc 色調を予約保存します。
@@ -139,79 +143,81 @@
  * @desc ウェイト時間(フレーム：1/60秒)
  * @default 60
  *
- @ -------------------------- プラグインコマンド
+ @ --------------------------
  * @command RestorationSetTint
  * @text 色調の予約を反映
  * @desc 色調を予約した内容に変更します。
+ * 
+ * @arg Wait
+ * @text ウェイト時間(フレーム)
+ * @type string
+ * @desc ウェイト時間(フレーム:1/60秒)
+ * @default 60
  *
  */
 // ----------------------------------------------------------------------------
-//　全体を関数で囲む
+// 全体を関数で囲む
 (() => {
     'use strict';
-//　プラグインパラメーターを変数にして取り込む
-const pluginName = 'OKAS_SaveChangeTint';
-const parameters = PluginManager.parameters(pluginName);
-//　ローカル変数の設定　(*このプラグイン内でのみ有効な変数)
-const SaveTV = parseInt(parameters['Save Tint Variable']) || 1;
-const SetTV = parseInt(parameters['Set Tint Variable']) || 2;
+    // プラグインパラメーターを変数にして取り込む
+    const pluginName = 'OKAS_SaveChangeTint';
+    const parameters = PluginManager.parameters(pluginName);
+    // ローカル変数の設定　(*このプラグイン内でのみ有効な変数)
+    const SaveTV = parseInt(parameters['Save Tint Variable']) || 1;
+    const SetTV = parseInt(parameters['Set Tint Variable']) || 2;
 
-// =============================================================================
-// プラグインコマンド  現在の色調を保存&変更
-// =============================================================================
-PluginManager.registerCommand(pluginName, "SaveChangeTint", function(args) {
-    Game_Map.prototype.saveTint();
-    const p = {};
-    p.red = Number(args.Red, 0);
-    p.green = Number(args.Green, 0);
-    p.blue = Number(args.Blue, 0);
-    p.gray = Number(args.Gray, 0);
-    p.wait = Number(args.Wait, 0);
-    $gameScreen.startTint([p.red,p.green,p.blue,p.gray], p.wait);
-});
+    // =============================================================================
+    // プラグインコマンド
+    // =============================================================================
 
-// =============================================================================
-// プラグインコマンド  現在の色調を保存　save
-// =============================================================================
-PluginManager.registerCommand(pluginName, "SaveTint", function() {
-    Game_Map.prototype.saveTint();
-});
+    // 現在の色調を保存&変更
+    PluginManager.registerCommand(pluginName, "SaveChangeTint", function (args) {
+        Game_Map.prototype.saveTint();
+        const p = {};
+        p.red = Number(args.Red) || 0;
+        p.green = Number(args.Green) || 0;
+        p.blue = Number(args.Blue) || 0;
+        p.gray = Number(args.Gray) || 0;
+        p.wait = Number(args.Wait) || 60;
+        $gameScreen.startTint([p.red, p.green, p.blue, p.gray], p.wait);
+    });
 
-// =============================================================================
-// プラグインコマンド  色調の復元
-// =============================================================================
-PluginManager.registerCommand(pluginName, "RestorationTint", function() {
-    const v = $gameVariables.value(SaveTV);
-      $gameScreen.startTint([v[0],v[1],v[2],v[3]], v[4]);
-});
+    // 現在の色調を保存　save
+    PluginManager.registerCommand(pluginName, "SaveTint", function () {
+        Game_Map.prototype.saveTint();
+    });
 
-// =============================================================================
-// プラグインコマンド  色調の予約 set
-// =============================================================================
-PluginManager.registerCommand(pluginName, "SetTint", function(args) {
-    const p = {};
-    p.red = Number(args.Red, 0);
-    p.green = Number(args.Green, 0);
-    p.blue = Number(args.Blue, 0);
-    p.gray = Number(args.Gray, 0);
-    p.wait = Number(args.Wait, 0);
-    $gameVariables._data[SetTV] = [p.red,p.green,p.blue,p.gray,p.wait];
-});
+    // 色調の復元
+    PluginManager.registerCommand(pluginName, "RestorationTint", function (args) {
+        const v = $gameVariables.value(SaveTV);
+        const wait = Number(args.Wait) || 60;
+        $gameScreen.startTint([v[0], v[1], v[2], v[3]], wait);
+    });
 
-// =============================================================================
-// プラグインコマンド  色調の予約を反映
-// =============================================================================
-PluginManager.registerCommand(pluginName, "RestorationSetTint", function() {
-    const v = $gameVariables.value(SetTV);
-      $gameScreen.startTint([v[0],v[1],v[2],v[3]], v[4]);
-});
+    // 色調の予約 set
+    PluginManager.registerCommand(pluginName, "SetTint", function (args) {
+        const p = {};
+        p.red = Number(args.Red) || 0;
+        p.green = Number(args.Green) || 0;
+        p.blue = Number(args.Blue) || 0;
+        p.gray = Number(args.Gray) || 0;
+        p.wait = Number(args.Wait) || 60;
+        $gameVariables._data[SetTV] = [p.red, p.green, p.blue, p.gray, p.wait];
+    });
 
-// ------------------------------------------------------------------------------
-// saveTint()
-// ------------------------------------------------------------------------------
-Game_Map.prototype.saveTint = function() {
-    $gameVariables._data[SaveTV] = [$gameScreen._tone[0], $gameScreen._tone[1], $gameScreen._tone[2], $gameScreen._tone[3], 60];
-};
+    // 色調の予約を反映
+    PluginManager.registerCommand(pluginName, "RestorationSetTint", function (args) {
+        const v = $gameVariables.value(SetTV);
+        const wait = Number(args.Wait) || 60;
+        $gameScreen.startTint([v[0], v[1], v[2], v[3]], wait);
+    });
+
+    // ------------------------------------------------------------------------------
+    // saveTint()
+    // ------------------------------------------------------------------------------
+    Game_Map.prototype.saveTint = function () {
+        $gameVariables._data[SaveTV] = [$gameScreen._tone[0], $gameScreen._tone[1], $gameScreen._tone[2], $gameScreen._tone[3], 60];
+    };
 
 
 })();
